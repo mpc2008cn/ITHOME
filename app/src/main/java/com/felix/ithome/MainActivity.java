240bc0c754a6da3a868d264ba347031fa2467db7
Felix <mpc2008cn@gmail.com>
2016-05-19 18:28:55 +0800

    Add tablayout

--- app/src/main/java/com/felix/ithome/MainActivity.java
+++ app/src/main/java/com/felix/ithome/MainActivity.java
@@ -2,9 +2,9 @@ package com.felix.ithome;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("最新"));
+        tabLayout.addTab(tabLayout.newTab().setText("排行榜"));
+        tabLayout.addTab(tabLayout.newTab().setText("安卓"));
+        tabLayout.addTab(tabLayout.newTab().setText("手机"));
+        tabLayout.addTab(tabLayout.newTab().setText("数码"));
     }
 
     @Override
