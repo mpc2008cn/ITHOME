8862d3223d7be35e6d0d94f741e4a2b5d578d745
Felix <mpc2008cn@gmail.com>
2016-05-21 20:05:47 +0800

    Add tablayout

--- app/src/main/java/com/felix/ithome/MainActivity.java
+++ app/src/main/java/com/felix/ithome/MainActivity.java
@@ -6,17 +6,22 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private final int COUNT = 8;//标签数量
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //信封小图标
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        //左侧导航栏
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,13 +49,21 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //多标签
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        /*由viewpager来设定
         tabLayout.addTab(tabLayout.newTab().setText("最新"));
         tabLayout.addTab(tabLayout.newTab().setText("排行榜"));
         tabLayout.addTab(tabLayout.newTab().setText("安卓"));
         tabLayout.addTab(tabLayout.newTab().setText("手机"));
         tabLayout.addTab(tabLayout.newTab().setText("数码"));
+        */
+
+        ViewPager viewPager=(ViewPager)findViewById(R.id.viewPager);
+        viewPager.setAdapter(new MyViewPagerAdapter(this));
+        tabLayout.setupWithViewPager(viewPager);
     }
+
 
     @Override
     public void onBackPressed() {
@@ -88,24 +103,74 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        switch (item.getItemId()) {
+            case R.id.nav_info:
+            case R.id.nav_circle:
+            case R.id.nav_account:
+            case R.id.nav_settings:
+            case R.id.nav_about:
+            }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private  class MyViewPagerAdapter extends PagerAdapter {
+        private AppCompatActivity activity;
+
+        public MyViewPagerAdapter(MainActivity activity) {
+            this.activity=activity;
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int pos){
+            switch (pos){
+                case 0:
+                    return "最新";
+                case 1:
+                    return "排行榜";
+                case 2:
+                    return "安卓";
+                case 3:
+                    return "手机";
+                case 4:
+                    return "数码";
+                case 5:
+                    return "苹果";
+                case 6:
+                    return "WP";
+                case 7:
+                    return "Windows";
+                default:
+                    return "最新";
+            }
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container,int pos){
+            TextView tv=new TextView(activity);
+            tv.setText("@ViewPager:"+pos);
+            tv.setTextSize(30.0f);
+            tv.setGravity(Gravity.CENTER);
+            container.addView(tv);
+            return tv;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container,int position,Object object){
+            container.removeView((View)object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+
+        @Override
+        public int getCount(){
+            return COUNT;
+        }
     }
 }
